7ad2205776c9a2fdf02397d2cf2d1d8a91ea1e59
Lyly Long <lylylong@gmail.com>
2021-01-11 21:02:31 -0500

    add comment function

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -2,8 +2,16 @@ const { Pizza } = require("../models");
 
 const pizzaController = {
   // get all pizzas
+  // use select option inside & outside of populate(), so Mongoose will ignore __v fields
+  // .sort({ _id: -1 }) to sort in DESC order by the _id value
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -14,8 +22,12 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
-        // If no pizza is found, send 404
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
           return;
@@ -39,6 +51,7 @@ const pizzaController = {
   updatePizza({ params, body }, res) {
     // If we don't set that third parameter, { new: true }, it will return the original document.
     // By setting the parameter to true, we're instructing Mongoose to return the new version of the document.
+    // With Mongoose, the "where" clause is used first, then the updated data, then options for how the data should be returned.
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true }) // { new: true }, it will return the original document
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,56 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+
+    // 当决定加comments功能时，就需要连接两个models！
+    // How do you think we define the type so that Mongoose knows to expect a comment?
+    // We'll tell Mongoose to expect the type to be an ObjectId.
+    // The ref property is especially important ---
+    // --- because it tells the Pizza model which documents to search to find the right comments.
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+
+  // we need to tell the schema that it can use virtuals
+  // We set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
